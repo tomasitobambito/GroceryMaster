2477ab274119580c49fe57ac2a0d7a917f830aa9
tomasitobambito <tomgacek0604@gmail.com>
2022-02-18 12:52:30 +0100

    added reusable base and handler classes

---
+++ GroceryMaster/Handlers/FileHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace GroceryMaster.Handlers
+{
+    public static class FileHandler
+    {
+        public static string GetAppData()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            path = Path.Combine(path, "GroceryMaster");
+
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+            return path;
+        }
+
+        /// <summary>
+        /// Reads file, only call inside try catch block to ensure no FileNotFound Errors.
+        /// </summary>
+        /// <param name="path">Path of JSON file to read.</param>
+        /// <typeparam name="T">Object to serialize to.</typeparam>
+        /// <returns>Serialized object of type T.</returns>
+        public static T ReadFromJSONFile<T>(string path)
+        {
+            var fileText = File.ReadAllText(path);
+            if (fileText == "") fileText = "{}";
+
+            return JsonSerializer.Deserialize<T>(fileText);
+        }
+
+        public static void WriteToFile(string path, object objectToSerialize)
+        {
+            var jsonString = JsonSerializer.Serialize(objectToSerialize);
+            File.WriteAllText(path, jsonString);
+        }
+    }
+}
