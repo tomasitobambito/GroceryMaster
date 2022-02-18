3e96630c59ef579a4909196fd4437b52bf0b8718
tomasitobambito <tomgacek0604@gmail.com>
2022-02-18 11:21:03 +0100

    initial commit

---
+++ GroceryMaster/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using GroceryMaster.View;
+
+namespace GroceryMaster
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var win = new MainWindowView
+            {
+                Title = "Grocery Master"
+            };
+            win.Show();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Grocery Master
+This is a grocery management software written in C# and XAML using .NET 5 and WPF. The software was written as an [IBCS HL IA](## "International Baccalaureate Computer Science Higher Level Internal Assessment) .
