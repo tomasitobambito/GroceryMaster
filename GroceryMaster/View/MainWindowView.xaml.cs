4ecff49ef04b65d10eb276adb4f2f6030ec9cc5b
tomasitobambito <tomgacek0604@gmail.com>
2022-03-22 17:28:34 +0100

    it's that other device guy

--- GroceryMaster/View/MainWindowView.xaml.cs
+++ GroceryMaster/View/MainWindowView.xaml.cs
@@ -2,6 +2,8 @@
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using GroceryMaster.Model;
 using GroceryMaster.ViewModel;
 
 namespace GroceryMaster.View
@@ -18,6 +20,12 @@ namespace GroceryMaster.View
 
             DataContext = viewModel;
             InitializeComponent();
+
+            CollectionView storageView = (CollectionView) CollectionViewSource.GetDefaultView(LvStorage.ItemsSource);
+            storageView.Filter = ItemFilter;
+            CollectionView shoppingView = (CollectionView) CollectionViewSource.GetDefaultView(LvShopping.ItemsSource);
+            shoppingView.Filter = ItemFilter;
+
         }
 
         private void MainWindowView_OnClosed(object? sender, EventArgs e)
@@ -26,6 +34,47 @@ namespace GroceryMaster.View
         }
 
         private void ComboBoxStorage_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            StorageSortChanged();
+        }
+
+        private void ComboBoxShopping_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!ComboBoxShopping.IsLoaded) return;
+
+            var dir = ((string) ((ComboBoxItem) ComboBoxShopping.SelectedItem).Content).Split("-")[1]
+                      == "Ascending" ? ListSortDirection.Ascending : ListSortDirection.Descending;
+            
+            LvShopping.Items.SortDescriptions.Clear();
+            LvShopping.Items.SortDescriptions.Add(new SortDescription("Description", dir));
+        }
+
+        private void TextFilter_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(viewModel.SelectedTabIndex == 0 ? 
+                    LvStorage.ItemsSource : LvShopping.ItemsSource).Refresh();
+        }
+
+        private bool ItemFilter(object item)
+        {
+            if (string.IsNullOrEmpty(TextFilter.Text)) return true;
+
+            if (viewModel.SelectedTabIndex == 0)
+            {
+                return ((StorageItem) item).Description
+                    .IndexOf(TextFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0;
+            }
+
+            return ((ShoppingItem) item).Description
+                .IndexOf(TextFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        private void LvStorage_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            StorageSortChanged();
+        }
+
+        private void StorageSortChanged()
         {
             if (!ComboBoxStorage.IsLoaded) return;
 
@@ -43,17 +92,6 @@ namespace GroceryMaster.View
                 LvStorage.Items.SortDescriptions.Add(new SortDescription(args[0], dir));
                 LvStorage.Items.SortDescriptions.Add(new SortDescription("Description", ListSortDirection.Ascending));
             }
-        }
-
-        private void ComboBoxShopping_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (!ComboBoxShopping.IsLoaded) return;
-
-            var dir = ((string) ((ComboBoxItem) ComboBoxShopping.SelectedItem).Content).Split("-")[1]
-                      == "Ascending" ? ListSortDirection.Ascending : ListSortDirection.Descending;
-            
-            LvShopping.Items.SortDescriptions.Clear();
-            LvShopping.Items.SortDescriptions.Add(new SortDescription("Description", dir));
         }
     }
 }
