a560b1c06625e1ae0e499d2572a11b96d2357922
tomasitobambito <tomgacek0604@gmail.com>
2022-03-28 17:50:35 +0200

    6

--- GroceryMaster/Converters/DateTimeToDateStringConverter.cs
+++ GroceryMaster/Converters/DateTimeToDateStringConverter.cs
@@ -6,14 +6,16 @@ namespace GroceryMaster.Converters
 {
     public class DateTimeToDateStringConverter : IValueConverter
     {
+        // Convert from DateTime to String
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value?.ToString()?.Split(" ")[0];
+            return value?.ToString()?.Split(" ")[0]; // since DateTime is nullable the return of this function must be too
         }
-
+        
+        // Convert from String to DateTime
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null ? null : DateTime.Parse((string) value);
+            return value == null ? null : DateTime.Parse((string) value); // since value is nullable, the return must account for it
         }
     }
 }
