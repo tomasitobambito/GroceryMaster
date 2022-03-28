377023d22e8cd4c0f3fccc54d4fd11c0c562c051
tomasitobambito <tomgacek0604@gmail.com>
2022-03-28 15:32:39 +0200

    3

--- GroceryMaster/AttachedProperties/SelectedItemsBehavior.cs
+++ GroceryMaster/AttachedProperties/SelectedItemsBehavior.cs
@@ -8,13 +8,15 @@ namespace GroceryMaster.AttachedProperties
         private readonly ListView _listView;
         private readonly IList _boundList;
 
+        // constructor receives ListView object and IList that are copied into private fields
         public SelectedItemsBehavior(ListView listView, IList boundList)
         {
             _boundList = boundList;
             _listView = listView;
-            _listView.SelectionChanged += OnSelectionChanged;
+            _listView.SelectionChanged += OnSelectionChanged; // subscribe to OnSelectionChanged event of ListView
         }
 
+        // Method to update bound list whenever ListView changes (i.e. selection changes)
         private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             _boundList.Clear();
